b267adcb84f1ed721830c163a9156f4186e0b4bf
Jeremy DeLong <jdelong@backofficethinking.com>
2019-03-21 15:51:09 -0400

    adjusting again

--- web/sites/default/setttings.migrate-on-pantheon.php
+++ web/sites/default/setttings.migrate-on-pantheon.php
@@ -1,22 +1,31 @@
 <?php
+/**
+ * Get secrets from secrets file.
+ *
+ * @param array $requiredKeys  List of keys in secrets file that must exist.
+ */
 
 $secretsFile = $_SERVER['HOME'] . '/files/private/secrets.json';
 if (file_exists($secretsFile)) {
- $secrets = json_decode(file_get_contents($secretsFile), 1);
+    $secrets = json_decode(file_get_contents($secretsFile), 1);
+} else {
+    die('No secrets file found. Aborting!');
+}
+if ($secrets == false) {
+    die('Could not parse json in secrets file. Aborting!');
 }
 if (!empty($secrets['migrate_source_db__url'])) {
- $parsed_url = parse_url($secrets['migrate_source_db__url']);
- echo $parsed_url . " = Parsed URL VARIABLE";
- if (!empty($parsed_url['port']) && !empty($parsed_url['host']) && !empty($parsed_url['pass'])) {
- $databases['drupal_7']['default'] = array (
-   'database' => 'pantheon',
-   'username' => 'pantheon',
-   'password' => $parsed_url['pass'],
-   'host' => $parsed_url['host'],
-   'port' => $parsed_url['port'],
-   'driver' => 'mysql',
-   'prefix' => '',
-   'collation' => 'utf8mb4_general_ci',
- );
- }
+    $parsed_url = parse_url($secrets['migrate_source_db__url']);
+    if (!empty($parsed_url['port']) && !empty($parsed_url['host']) && !empty($parsed_url['pass'])) {
+        $databases['drupal_7']['default'] = array (
+        'database' => 'pantheon',
+        'username' => 'pantheon',
+        'password' => $parsed_url['pass'],
+        'host' => $parsed_url['host'],
+        'port' => $parsed_url['port'],
+        'driver' => 'mysql',
+        'prefix' => '',
+        'collation' => 'utf8mb4_general_ci',
+        );
+    }
 }
